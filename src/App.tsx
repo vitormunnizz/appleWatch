7d264ebad879ffb239dc7385924f22b6ede310b8
vitormunnizz <vitormunnizzdev@gmail.com>
2025-10-22 20:30:09 -0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,25 @@ function App() {
     <div>
       <header>
         <nav>
-          <ul className='flex gap-10 justify-center m-3 align-text-center'>
-            <li className='align-text-center'><FaApple className="text-1xl" /></li>
-            <li>Loja</li>
-            <li>Mac</li>
-            <li>IPad</li>
-            <li>IPhone</li>
-            <li>Watch</li>
-            <li>AirPods</li>
-            <li>Tv e Casa</li>
-            <li>Entreterimento</li>
-            <li>Acessórios</li>
-            <li>Suporte</li>
-            <li><PiMagnifyingGlassBold className="w-4 h-7 text-gray-700 hover:text-black transition" /></li>
-            <li><PiBagBold className="w-4 h-7 text-gray-700 hover:text-black transition" /></li>
+          <ul className='flex justify-center gap-10 items-center'>
+            <li className='cursor-pointer'><FaApple className="text-1xl" id='apple'/></li>
+            <li className='cursor-pointer'>Loja</li>
+            <li className='cursor-pointer'>Mac</li>
+            <li className='cursor-pointer'>IPad</li>
+            <li className='cursor-pointer'>IPhone</li>
+            <li className='cursor-pointer'>Watch</li>
+            <li className='cursor-pointer'>AirPods</li>
+            <li className='cursor-pointer'>Tv e Casa</li>
+            <li className='cursor-pointer'>Entreterimento</li>
+            <li className='cursor-pointer'>Acessórios</li>
+            <li className='cursor-pointer'>Suporte</li>
+            <li className='cursor-pointer'><PiMagnifyingGlassBold className="w-4 h-7 text-gray-700 hover:text-black transition" /></li>
+            <li className='cursor-pointer'><PiBagBold className="w-4 h-7 text-gray-700 hover:text-black transition" /></li>
           </ul>
         </nav>
       </header>
+
+      
     </div>
   )
 }
