c8784d6db6a44cd3e0a9c8703451c8109ea41cc2
vitormunnizz <vitormunnizzdev@gmail.com>
2025-10-26 20:12:59 -0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import { PiMagnifyingGlassBold, PiBagBold } from "react-icons/pi";
 import img0 from "./assets/opcoes-cores/imagens-azul-inverno/imagem-0.jpeg";
 import img1 from "./assets/opcoes-cores/imagens-azul-inverno/imagem-1.jpeg";
 import img2 from "./assets/opcoes-cores/imagens-azul-inverno/imagem-2.jpeg";
-import tema0 from "./assets/opcoes-cores/azul-inverno.jpeg";
+import cor0 from "./assets/opcoes-cores/azul-inverno.jpeg";
+import cor1 from "./assets/opcoes-cores/estelar.jpeg";
+import cor2 from "./assets/opcoes-cores/meia-noite.jpeg";
+import cor3 from "./assets/opcoes-cores/rosa-claro.jpeg";
+import cor4 from "./assets/opcoes-cores/verde-cipreste.jpeg";
 
 
 function App() {
@@ -38,8 +42,12 @@ function App() {
           <p> Em até 12x de R$ 95,75</p>
           <p>R$ 1.034,10 à vista (10% de desconto)</p>
           <p className='text-2xl my-5'>Cor - Azul-inverno</p>
-          <div id='imagens'>
-            <img src={tema0} alt="" width={40}/>
+          <div id='imagens' className='flex gap-5'>
+            <img src={cor0} alt="" width={35} className='cursor-pointer'/>
+            <img src={cor1} alt="" width={35} className='cursor-pointer'/>
+            <img src={cor2} alt="" width={35} className='cursor-pointer'/>
+            <img src={cor3} alt="" width={35} className='cursor-pointer'/>
+            <img src={cor4} alt="" width={35} className='cursor-pointer'/>
           </div>
           <p>Cor da caixa do Apple Watch - Preto</p>
           <h2>Tamanho da caixa</h2>
